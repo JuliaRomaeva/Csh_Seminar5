0acde091101b052f1668a73a748fa43fd0505f88
JuliaRomaeva <julia.romaeva@gmail.com>
2023-02-28 21:45:04 +0300

    перевод строки в массив, поиск числа

---
+++ example33/Program.cs
@@ -0,0 +1,72 @@
+﻿// **Задача 33:** Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+// Console.Clear();
+// Console.Write("Введите элементы массива через пробел: ");
+// string elements = Console.ReadLine();
+// int[] baseArray = GetArrayFromString(elements);
+// Console.Write("Введите число: ");
+// int n = int.Parse(Console.ReadLine());
+// if (FindElement(baseArray, n))
+// {
+// Console.WriteLine("Да");
+// }
+// else
+// {
+// Console.WriteLine("Нет");
+// }
+
+
+// int[] GetArrayFromString(string stringArray)
+// {
+// string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+// int[] res = new int[nums.Length];
+
+// for (int i = 0; i < nums.Length; i++)
+// {
+// res[i] = int.Parse(nums[i]);
+// }
+// return res;
+// }
+
+// bool FindElement(int[] array, int el)//[1,2,3,4] 5
+// {
+// foreach (var item in array)//
+// {
+// if (el == item) // 5 == 4
+// return true;
+// }
+// return false;
+// }
+Console.Clear();
+Console.Write("Введите массив через пробел: ");
+string arr = Console.ReadLine();
+int[] array = GetArrayFromString(arr);
+
+Console.Write("Введите число, котрое нужно найти: ");
+int n = int.Parse(Console.ReadLine());
+
+int[] GetArrayFromString(string stringArray){
+string[] nums = stringArray.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+int[] res = new int[nums.Length];
+ for(int i = 0; i < nums.Length; i++ ){
+    res[i] = int.Parse(nums[i]);
+ }
+ return res;
+}
+
+bool FindElement(int[] array, int el){
+    foreach(var item in array){
+        if(el == item)
+        return true;
+    }
+    return false;
+}
+
+if (FindElement(array, n)){
+    Console.Write("да ");
+}
+else {
+    Console.Write("нет ");
+}
